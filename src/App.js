c1e47f0cba2566f6913681c4765a6b13875725ea
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-07 10:50:21 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './Components/Navbar';
+import{makeStyle} from 'material-ui/core/style';
+import{paper,TextField, Button,CircularProgress } from '@material-ui/core';
 import InfoPanel from './Components/InfoPanel';
-import barChart from './barChart';
+import barChart from './barChart'
 function App() {
   return (
     <div>
      <Navbar/>
 
      <InfoPanel/>
-     
+
      <barChart/>
     </div>
   );
