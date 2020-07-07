40d3235efc881b16005f86e051d4d808581fbbe1
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-07 21:19:20 +0500

    update task

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import Navbar from './Components/Navbar';
-import{paper,TextField, Button,CircularProgress } from '@material-ui/core';
-import InfoPanel from './Components/InfoPanel';
-function App() {
+import {Cards, Chart,CountryPicker} from './Components';
+import styles from './App.module.css';
+
+//import Navbar from './Components/Navbar';
+//import{paper,TextField, Button,CircularProgress } from '@material-ui/core';
+//import InfoPanel from './Components/InfoPanel';
+//function App() {
+class App extends React.Component {  
+  render(){
   return (
-    <div>
-     <Navbar/>
-
-     <InfoPanel/>
-
+   
+<div className={styles.container}>
+  <Cards/>
+ <CountryPicker/>
+ <Chart/>
     </div>
   );
+  )
 }
 
 export default App;
