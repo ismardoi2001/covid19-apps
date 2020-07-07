4a0b41bf7cbd9a6da7d4e08bad612e954fa8964a
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-07 19:49:47 +0500

    update task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
-import{makeStyle} from 'material-ui/core/style';
 import{paper,TextField, Button,CircularProgress } from '@material-ui/core';
 import InfoPanel from './Components/InfoPanel';
-import barChart from './barChart'
 function App() {
   return (
     <div>
@@ -11,7 +9,6 @@ function App() {
 
      <InfoPanel/>
 
-     <barChart/>
     </div>
   );
 }
