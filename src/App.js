6ea07b8d7d8389258d095d9a09a6c666ccce66c2
Muhammad Ismail <ismardoi2001@gmail.com>
2020-07-07 10:12:13 +0500

    update task

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import InfoPanel from './Components/InfoPanel';
-
+import barChart from './barChart';
 function App() {
   return (
     <div>
      <Navbar/>
      <InfoPanel/>
+     <Navbar/>
     </div>
   );
 }
